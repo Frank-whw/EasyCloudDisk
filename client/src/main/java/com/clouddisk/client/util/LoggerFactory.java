e26458586a4fd8fde05af85758cdc6424c251377
LuYuan-Zjyy <your-email@example.com>
2025-11-09 20:45:12 +0800

    变成一个简单的封装：统一加上 [CloudDisk] 前缀，并通过 SLF4J 获取 Logger，方便全项目用一致的日志格式

--- client/src/main/java/com/clouddisk/client/util/LoggerFactory.java
+++ client/src/main/java/com/clouddisk/client/util/LoggerFactory.java
@@ -1,5 +1,30 @@
 package com.clouddisk.client.util;
 
-public class LoggerFactory {
-    // TODO: 统一日志格式，或引入 SLF4J
+import org.slf4j.Logger;
+
+/**
+ * 简单的日志工厂，统一添加项目前缀。
+ */
+public final class LoggerFactory {
+
+    private static final String PREFIX = "[CloudDisk] ";
+
+    private LoggerFactory() {
+    }
+
+    public static Logger getLogger(Class<?> type) {
+        return org.slf4j.LoggerFactory.getLogger(type);
+    }
+
+    public static Logger getLogger(String name) {
+        return org.slf4j.LoggerFactory.getLogger(name);
+    }
+
+    public static String format(String message) {
+        return PREFIX + message;
+    }
+
+    public static String format(String template, Object... args) {
+        return PREFIX + template.formatted(args);
+    }
 }
